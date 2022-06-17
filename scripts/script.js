a7617e61f6c922a0ca74cfa3edb4d9e2ef4579ed
Gpaneca3692 <gpaneca3692@gmail.com>
2022-06-17 16:34:53 -0400

    Create the page index

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 console.log("hello world");
+
+function computerPlay() {
+    var randomNumber = Math.random() * 10;
+    if (randomNumber < 4) return "ROCK";
+    else if (randomNumber < 7) return "SCISSORS";
+    else return "PAPER";
+}
